4eeb9fd0fd727ccde36b8510fbe157efb6822db5
Wenyuan Wu <wenyuanw@cs.utexas.edu>
2018-02-07 16:50:11 -0600

    attempt width

--- Assignment_1/energyfunc.m
+++ Assignment_1/energyfunc.m
@@ -1,3 +1,4 @@
 function[energy] = energyfunc(im)
     energy =abs(imfilter(im,[-1,0,1],'replicate'))+abs(imfilter(im,[-1;0;1],'replicate'));
 end
+    
--- Assignment_1/reduceWidth.m
+++ Assignment_1/reduceWidth.m
@@ -1,5 +1,27 @@
 function [output] = reduceWidth(im, numPixels)
-    im_gray=rgb2gray(im)
-    energy=energyfunc(im_gray)
-     
+    im_gray=rgb2gray(im);
+    energy=energyfunc(im_gray);
+    
+    % loop throught the image
+    [x,y]=size(energy);
+    disp(energy(x,:))
+    for i= 2:x
+        for j=1:y
+            % we purposely start at row 2. Modify each value so that it
+            % adds the lowest value above.
+            % take care of edge cases
+            if(j==1)
+                energy(i,j)=energy(i,j)+min([energy(i-1,j) energy(i-1,j+1)]);
+            elseif(j==y)
+                energy(i,j)=energy(i,j)+min([energy(i-1,j) energy(i-1,j-1)]);
+            else
+                energy(i,j)=energy(i,j)+min([energy(i-1,j-1) energy(i-1,j) energy(i-1,j-1)]);
+            end
+        end
+    end
+    disp(energy(x,:))
+    
+    
+    
+    output = 0;
 end
